962f342059a5f869f5af3ec8a60a6be74b504b0f
Colin Suckow <csuckow99@gmail.com>
2021-10-02 17:46:25 -0700

    Hack in depth thresholding for person segmentation

--- kinect_test/src/main.rs
+++ kinect_test/src/main.rs
@@ -1,5 +1,5 @@
 use freenectrs::freenect;
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, MouseButton, MouseMode, Window, WindowOptions};
 
 const WINDOW_WIDTH: usize = 640;
 const WINDOW_HEIGHT: usize = 480;
@@ -48,14 +48,43 @@ fn main() {
         // Fetch the video and depth frames
         if let Ok((data, _timestamp)) = dstream.receiver.try_recv() {
             for (i, p) in data.iter().enumerate() {
+
+                let pixel_index = (i / FRAME_WIDTH * WINDOW_WIDTH) + (i % FRAME_WIDTH);
+
+                // Threshold depth data
+                if *p > 900 || *p < 850 {
+                    buffer[pixel_index] = 0;
+                    continue;
+                }
+
                 let p_normalized = (p.clone() as f64) / 2048.0;
                 let pc = ((1.0 - p_normalized) * 255.0) as u32;
                 let pixel_val = pc | pc << 8 | pc << 16;
-                let pixel_index = (i / FRAME_WIDTH * WINDOW_WIDTH) + (i % FRAME_WIDTH);
+                
                 buffer[pixel_index] = pixel_val;
             }
+
+            //Display depth info at mouse cursor
+            window
+                .get_mouse_pos(MouseMode::Discard)
+                .map(|(mouse_x, mouse_y)| {
+                    for i in 0..buffer.len() {
+                        let x = i % FRAME_WIDTH;
+                        let y = i / FRAME_WIDTH;
+
+                        if mouse_x as usize == x || mouse_y as usize == y {
+                            buffer[i] = 0xFF << 18;
+                        }
+                    }
+
+                    let index = mouse_x as usize + mouse_y as usize * FRAME_WIDTH;
+
+                    if window.get_mouse_down(MouseButton::Left) {
+                        println!("Depth at mouse cursor: {}", data[index]);
+                    }
+                });
         }
-       
+
         window
             .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
             .unwrap();
