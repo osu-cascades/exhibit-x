8acaea50f01bdcd8b93929f4d259f3db7070f60c
Colin Suckow <csuckow99@gmail.com>
2021-10-11 19:59:51 -0700

    Win borrow checker deathmatch over kinect

--- kinect_nannou/src/main.rs
+++ kinect_nannou/src/main.rs
@@ -1,11 +1,13 @@
-use freenectrs::freenect;
+use kinect::Kinect;
 use nannou::prelude::*;
+
+mod kinect;
 
 fn main(){
     nannou::app(init_model).update(update).run();
 }
 
-fn init_model<'a>(app: &App) -> Model<'a>{
+fn init_model<'a, 'b>(app: &App) -> Model<'a, 'b>{
     let kinect = Kinect::new();
     let _window = app.new_window().view(view).build().unwrap();
 
@@ -21,35 +23,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.to_frame(app, &frame).unwrap();
 }
 
-struct Model<'a> {
+struct Model<'a, 'b> {
     _window: window::Id,
-    kinect: Kinect<'a>
+    kinect: Kinect<'a, 'b>
 } 
-
-struct Kinect<'a> {
-    dstream: freenect::FreenectDepthStream<'a, 'a>,
-    ctx: freenect::FreenectContext,
-    device: freenect::FreenectDevice<'a, 'a>
-}
-
-impl<'a> Kinect<'a> {
-    fn new() -> Kinect<'a> {
-        let ctx = freenect::FreenectContext::init_with_video().unwrap();
-        let device = ctx.open_device(0).unwrap();
-        device
-            .set_depth_mode(
-                freenect::FreenectResolution::Medium,
-                freenect::FreenectDepthFormat::Bit11,
-            )
-            .unwrap();
-        device
-            .set_video_mode(
-                freenect::FreenectResolution::Medium,
-                freenect::FreenectVideoFormat::Rgb,
-            )
-            .unwrap();
-        let dstream = device.depth_stream().unwrap();
-
-        Kinect {ctx, device, dstream}
-    }
-}
