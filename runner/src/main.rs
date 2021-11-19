6b4bcc625b49cc794c19d7263610df00db4d72d8
Colin Suckow <csuckow99@gmail.com>
2021-11-19 13:34:25 -0800

    Run formatter

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,12 +1,18 @@
-use std::{path::PathBuf, thread, time::Duration};
 use sketch::Sketch;
+use std::{path::PathBuf, thread, time::Duration};
 
 mod sketch;
 
 fn main() {
     let sketches = vec![
-        Sketch::new("trippy_colors".to_string(), PathBuf::from("/home/exhibitx/ExhibitX/exhibit-x/finshedSketches/trippy_colors")),
-        Sketch::new("face".to_string(), PathBuf::from("/home/exhibitx/ExhibitX/exhibit-x/finshedSketches/face")),
+        Sketch::new(
+            "trippy_colors".to_string(),
+            PathBuf::from("/home/exhibitx/ExhibitX/exhibit-x/finshedSketches/trippy_colors"),
+        ),
+        Sketch::new(
+            "face".to_string(),
+            PathBuf::from("/home/exhibitx/ExhibitX/exhibit-x/finshedSketches/face"),
+        ),
     ];
 
     loop {
@@ -18,5 +24,3 @@ fn main() {
         }
     }
 }
-
-
--- runner/src/sketch.rs
+++ runner/src/sketch.rs
@@ -1,4 +1,8 @@
-use std::{fmt::Display, path::PathBuf, process::{Child, Command, Stdio}};
+use std::{
+    fmt::Display,
+    path::PathBuf,
+    process::{Child, Command, Stdio},
+};
 
 pub struct Sketch {
     name: String,
@@ -11,10 +15,13 @@ impl Sketch {
     }
 
     /// Kills all running processing sketches
-    /// 
+    ///
     /// Individual sketches cannot be killed, so this method kills all sketches
     pub fn kill_all() {
-        Command::new("killall").arg("/home/exhibitx/.local/share/applications/processing-3.5.4/java/bin/java").spawn().unwrap();
+        Command::new("killall")
+            .arg("/home/exhibitx/.local/share/applications/processing-3.5.4/java/bin/java")
+            .spawn()
+            .unwrap();
     }
 
     fn path_str(&self) -> &str {
@@ -34,6 +41,11 @@ impl Sketch {
 
 impl Display for Sketch {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}, located at {}", self.name, self.path.to_string_lossy())
+        write!(
+            f,
+            "{}, located at {}",
+            self.name,
+            self.path.to_string_lossy()
+        )
     }
 }
